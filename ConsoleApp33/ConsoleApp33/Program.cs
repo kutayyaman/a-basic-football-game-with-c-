e435ac0c3ab4c2d284140ffbf422107f4d6400ce
kutayyaman <41294174+kutayyaman@users.noreply.github.com>
2020-02-19 23:40:22 +0200

    Update Program.cs

--- ConsoleApp33/ConsoleApp33/Program.cs
+++ ConsoleApp33/ConsoleApp33/Program.cs
@@ -5,18 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-/****************************************************************************
-**					SAKARYA ÜNİVERSİTESİ
-**				BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
-**				    BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
-**				   NESNEYE DAYALI PROGRAMLAMA DERSİ
-**					2014-2015 BAHAR DÖNEMİ
-**	
-**				ÖDEV NUMARASI..........: 2
-**				ÖĞRENCİ ADI............: Kutay Yaman
-**				ÖĞRENCİ NUMARASI.......: B171210074
-**                         DERSİN ALINDIĞI GRUP...: D
-****************************************************************************/
+
 namespace ConsoleApp33
 {
 	class Program
